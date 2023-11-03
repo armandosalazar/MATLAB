dc697a4f39268ba48d343b650000dd0a84de3404
Armando Salazar <armandosalazar351@gmail.com>
2023-11-03 17:01:40 -0600

    feat: read data from file

---
+++ Machine Learning/files.m
@@ -0,0 +1,9 @@
+% @author: Armando Salazar
+% @date: 03-11-23
+
+clear %limpiar el workspace
+%data = readtable("file.xlsx");
+%puedes utilizar la función readtable para importar datos tabulares de una
+%hoja de cálculo o un archivo de texto y almacenar el resultado como una
+%tabla.
+data=readtable("Machine Learning/file.txt");
