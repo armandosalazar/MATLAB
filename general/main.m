2352d4d4e0cd5773a63df1f8b4853e67d4e3e33d
Armando Salazar <armandosalazar351@gmail.com>
2023-11-03 13:57:07 -0600

    chore: add gitignore

--- general/main.m
+++ general/main.m
@@ -1,9 +1,12 @@
-% Esto es un comentario
+% @author: Armando Salazar
+% @date: 03-11-23
+
+%Esto es un comentario
 a = 5; %La variable a se define como 5
-fprintf('The var is %d\n', a);
-% clear, clc
+fprintf('The var is %d\n', a); %imprime en pantalla y formatea
+
 m = 5*5;
-disp(m);
-clc % borrar la ventana de comandos
-disp("Hi");
+disp(m); %mostrar el valor de una variable por pantalla
+clc %borrar la ventana de comandos
 a = 10 % añadir ; para ocultar el output
+clear a m % eliminar los elementos de el área de trabajo (workspace)
