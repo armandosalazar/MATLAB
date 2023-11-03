52c43cf0ac0185679d69ba26fbb78aeca73a142c
Armando Salazar <armandosalazar351@gmail.com>
2023-11-03 13:51:09 -0600

    chore: initial commit

---
+++ general/main.m
@@ -0,0 +1,9 @@
+% Esto es un comentario
+a = 5; %La variable a se define como 5
+fprintf('The var is %d\n', a);
+% clear, clc
+m = 5*5;
+disp(m);
+clc % borrar la ventana de comandos
+disp("Hi");
+a = 10 % añadir ; para ocultar el output
