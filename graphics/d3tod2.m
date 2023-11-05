ee39999867821f7b814107061406727d9ab810fc
Armando Salazar <armandosalazar351@gmail.com>
2023-11-05 12:17:44 -0600

    feat: 3d to 2d working

---
+++ graphics/d3tod2.m
@@ -0,0 +1,8 @@
+% @author: Armando Salazar
+% @date: 05-11-23
+
+t = 0:pi/50:10*pi;
+st=sin(t);
+ct=cos(t);
+plot3(st,ct,t);
+table = table()
--- graphics/nano.m
+++ graphics/nano.m
@@ -1,27 +1,46 @@
 % @author: Armando Salazar
 % @date: 03-11-23
 
+
 clc, clear
 %for var = inicio:fin
 %for var = inicio:incremento:fin
 values_t = zeros(1,51);
 values_x = zeros(1,51);
 values_y = zeros(1,51);
-vector = [4, 5, 7];
+%vector = [2, 3, 2]; %vector de proyección
+vector = [1, 3, 25]; %vector de proyección
+
 
 inc = 8 * pi / 50;
 i = 1;
 for t = 0:inc:8*pi
     %fprintf('%f\n', t);
-    % values_t = [values_t, t]; %aÃ±adir al final
+    %values_t = [values_t, t]; %añadir al final
+    %parametricas
+    % x = x_1 + x_p * u = -z_1/z_p
+    % y = y_1 + y_p * u = -z_1/z_p
+    % z = z_1 + z_p * u = -z_1/z_p
+    u = -t/vector(3);
+    x = cos(t) + vector(1)*u;
+    y = sin(t) + vector(2)*u;
+    % z = t + vector(3)*u;
+
     values_t(i) = t;
-    values_x(i) = cos(t);
-    values_y(i) = sin(t);
+    values_x(i) = x;
+    values_y(i) = y;
+
+    % values_t(i) = t;
+    % values_x(i) = cos(t);
+    % values_y(i) = sin(t);
     i = i +1;
 end
+clear u vector x y z
 clear i inc t
 table = table(values_t', values_x', values_y', values_t', 'VariableNames', {'t', 'x', 'y', 'z'});
 clear values_t values_x values_y
 %writetable(table, 'graphics/nombre_archivo.xlsx', 'Sheet','Hoja1');
 %clear table
+plot(table.x, table.y);
+%axis equal
 disp('Successfully!!');
