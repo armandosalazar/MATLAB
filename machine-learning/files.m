7ce5fddd8b41afff9923cf9f43676355bda29b4b
Armando Salazar <armandosalazar351@gmail.com>
2023-11-04 21:04:36 -0600

    feat: read data from file and plot

--- machine-learning/files.m
+++ machine-learning/files.m
@@ -1,9 +1,14 @@
 % @author: Armando Salazar
 % @date: 03-11-23
 
+clc %limpiar command window
 clear %limpiar el workspace
 %data = readtable("file.xlsx");
 %puedes utilizar la función readtable para importar datos tabulares de una
 %hoja de cálculo o un archivo de texto y almacenar el resultado como una
 %tabla.
-data=readtable("Machine Learning/file.txt");
+table=readtable("machine-learning/file.txt");
+x=table.x; %obtener el contenido de la columna x.
+disp(x);
+
+plot(x, table.y)
