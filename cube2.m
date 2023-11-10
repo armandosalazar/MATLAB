85ca4d292f9a5fc0a51179a594242e5624ef6630
Armando Salazar <armandosalazar351@gmail.com>
2023-11-10 13:30:36 -0600

    feat: add perceptron

--- cube2.m
+++ cube2.m
@@ -17,6 +17,7 @@ for i=1:size(points,1)
     u=-vector(3)/(points(i,3)-vector(3));
     points(i,1)=vector(1)+(points(i,1)-vector(1))*u;
     points(i,2)=vector(2)+(points(i,2)-vector(2))*u;
+    points(i,3)=vector(3)+(points(i,3)-vector(3))*u;
     fprintf('x:%.2f\ty:%.2f\n', points(i,1), points(i,2));
 end
 plot(points(:,1),points(:,1));
---
+++ perceptron.m
@@ -0,0 +1,17 @@
+% @autor: Armando Salazar
+% @date: 07-11-23
+
+clc, clear, close all
+p1=[1;2];
+p2=[-1;2];
+p3=[0;-1];
+p4=[1;-1.5];
+
+P=[p1 p2 p3 p4];
+
+w=rand(1,2);
+b=rand(1);
+
+T = [1 0 0 1];
+
+plotpv(P, T)
