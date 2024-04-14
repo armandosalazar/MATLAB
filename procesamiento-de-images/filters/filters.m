83fb48bfc7a012bb590c76d4381e2309cb860c09
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-04-14 10:57:05 -0600

    feat: all filters working

--- procesamiento-de-images/filters/filters.m
+++ procesamiento-de-images/filters/filters.m
@@ -4,8 +4,21 @@
 
 clear; clc; close all;
 
-I = imread("input/lenna.png");
+% I = imread("input/lenna.png");
+I = imread("input/mona.jpeg");
 
 IGray = I(:,:,1) / 3 + I(:,:,2) / 3 + I(:,:,3) / 3;
 
-ILaplace = laplace(IGray);
+% smoothing
+% IS = smoothing(IGray);
+% IS = box(IGray);
+% IS = gaussian(IGray);
+% IS = laplace(IGray);
+
+figure(1);
+subplot(1,2,1);
+imshow(IGray);
+title("Original Image");
+subplot(1,2,2);
+imshow(IS);
+title("Smoothed Image");
